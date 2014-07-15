5743a698e3a27392d5a3c12959950dace7d740b4
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-07-15 16:34:15 +0200

    Use https for youtube videos

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -21,7 +21,7 @@ class YouTube < Liquid::Tag
 
   def render(context)
     # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<center><iframe width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" src=\"http://www.youtube.com/embed/#{@id}\">&nbsp;</iframe></center>"
+    "<center><iframe width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" src=\"https://www.youtube.com/embed/#{@id}\"></iframe></center>"
   end
 
   Liquid::Template.register_tag "youtube", self
